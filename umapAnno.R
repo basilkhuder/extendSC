37aafed1519947662fdca816f79af4a58c3e9388
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-02-29 03:53:32 -0600

    Create umapAnno.R

---
+++ umapAnno.R
@@ -0,0 +1,24 @@
+# Takes a Seurat object and returns a UMAP embedding with cell counts as labels
+
+umapAnno <- function(seurat.obj,
+                     point.size = 1,
+                     label.size = 8){  
+  
+  `%>%` <- magrittr::`%>%`
+  extract.clusters <- data.table::setDT(FetchData(seurat.obj, vars = c("seurat_clusters")), 
+                                        keep.rownames = TRUE)
+  
+  cluster.counts <- extract.clusters %>%
+    dplyr::group_by(seurat_clusters) %>%
+    dplyr::tally()
+  
+  new.cluster.ids <- condition.extract.count$n
+  names(x = new.cluster.ids) <- levels(x = seurat.obj)
+  seurat.obj <- RenameIdents(object = seurat.obj, new.cluster.ids)
+  print(DimPlot(object = seurat.obj, 
+          reduction = 'umap', 
+          pt.size = point.size, 
+          label = TRUE, 
+          label.size = label.size))
+  seurat.obj@active.ident <- seurat.obj$seurat_clusters
+} 
