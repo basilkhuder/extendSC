a9416dde5dde18358ac812826e67c66c4af80de8
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-05 13:50:46 -0600

    Create featureFiltration.R

---
+++ R/featureFiltration.R
@@ -0,0 +1,9 @@
+#Filters a Seurat object based upon mitochondrial and feature percentages. Instead of the traditional approach
+to of arbitrarily filtering on a fixed %, filtering is based upon quantile percentages. 
+
+featureFiltration <- function(seurat.obj, parameters){
+
+
+
+
+}
