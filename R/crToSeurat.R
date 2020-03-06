ee7a6697b79babee28e7b5f4cebc8875c8b43526
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-06 17:08:55 -0600

    Update crToSeurat.R

--- R/crToSeurat.R
+++ R/crToSeurat.R
@@ -1,12 +1,8 @@
 # Takes a directory with standard CellRanger counts output (raw/filtered/analysis) and returns a list of Seurat objects.
-# With the merge parameter, object list can be merged with sample ids provided
+# With the merge parameter, object list can be merged with sample ids provided.
+# A parameters JSON file contains all Seurat parameters. 
 
-crToSeurat <- function(directory, 
-                       cells = 3,
-                       features = 200,
-                       sample.names = NULL,
-                       merge = FALSE){ 
-  
+crToSeurat <- function(directory, parameters, sample.names = NULL, merge = FALSE){ 
   `%>%` <- magrittr::`%>%`
   folders <- list.dirs(directory, recursive = FALSE)
   matrix.list <- vector(mode = "list", length = length(folders))
@@ -18,18 +14,21 @@ crToSeurat <- function(directory,
     features <- read.delim(paste(full.dir,list.files(path = full.dir, pattern = "features"),sep = "/"), 
                            header = FALSE,
                            stringsAsFactors = FALSE)
-    barcodes <- read.delim(paste(full.dir,list.files(path = full.dir, pattern = "barcode"),sep = "/"),
+    barcodes <- read.delim(paste(full.dir,
+                                 list.files(path = full.dir, pattern = "barcode"),sep = "/"),
                            header = FALSE,
                            stringsAsFactors = FALSE)
     matrix.list[[i]] <- matrix.list[[i]] %>%
       set_colnames(barcodes$V1) %>%
       set_rownames(features$V2) %>%
-      CreateSeuratObject(min.cells = cells, 
-                         min.features = features,
-                         project = sample.names[i])
+      CreateSeuratObject(min.cells = parameters[["min.cells"]],
+                         min.features = parameters[["min.features"]],
+                         project = sample.names[i] )
   }
   if (isTRUE(merge)){
-    matrix.list <- merge(matrix.list[[1]], y = matrix.list[-1], add.cell.ids = sample.names)
-  } 
+    matrix.list <- merge(matrix.list[[1]], 
+                         y = matrix.list[-1], 
+                         add.cell.ids = sample.names)
+  }
   return(matrix.list)
 }
