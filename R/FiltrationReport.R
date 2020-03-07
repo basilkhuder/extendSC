d1430eea7e624ea2cf9c83ef5cbd2d40d405c18e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-07 04:26:15 -0600

    Create FiltrationReport.R

---
+++ R/FiltrationReport.R
@@ -0,0 +1,10 @@
+# Generates a Filtration Report with n = QC plots based upon a range of r filtration values
+
+FiltrationReport <- function(seurat.obj, n = 10, range = .05) {
+  featureFiltration(seurat.obj) 
+
+
+
+
+
+} 
