922fada91314ed18e8a04286dab5bebddbc68610
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-09 16:35:39 -0500

    Delete FiltrationReport.R

--- R/FiltrationReport.R
+++
@@ -1,8 +0,0 @@
-# Generates a Filtration Report with n = QC plots based upon a range of r filtration values
-
-FiltrationReport <- function(seurat.obj, 
-                             n = 10, 
-                             range = .05) {
-
-
-} 
