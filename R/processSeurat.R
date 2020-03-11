006c36c4ba0e603b149ce20d03ac0392fecf82b9
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-11 13:42:13 -0500

    Update processSeurat.R

--- R/processSeurat.R
+++ R/processSeurat.R
@@ -4,16 +4,13 @@ processSeurat <- function(object = seurat.obj,
                           seed.use = 24,
                           n.cores = NULL) {
   `%>%` <- magrittr::`%>%`
-  
   if(!is.null(n.cores)){
     ifelse(n.cores > future::availableCores()[[1]], 
            stop("n.cores is greater than available system cores"),
            future::plan("multicore", workers = n.cores))
   }
-  
   seurat.obj <- seurat.obj %>%
-                Seurat::SCTransform(vars.to.regress = c("percent.mt","orig.ident"),
-                            verbose = FALSE) %>%
+                Seurat::SCTransform(vars.to.regress = c("percent.mt","orig.ident"), verbose = FALSE) %>%
                 Seurat::RunPCA() %>%
                 Seurat::FindNeighbors(dims = dims) %>%
                 Seurat::RunUMAP(reduction = "pca", dims = dims, seed.use = seed.use)
