bb059034327ce6a8def52c7a796fad4ce406d6e4
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-11 01:16:04 -0500

    Update featureFiltration.R

--- R/featureFiltration.R
+++ R/featureFiltration.R
@@ -2,17 +2,20 @@
 # of arbitrarily filtering on a fixed %, filtering is based upon quantile percentages. 
 # If seurat.obj is made up of multiple samples, each sample must have unique orig.ident
 
-featureFiltration <- function(seurat.obj, parameters){
+featureFiltration <- function(seurat.obj,
+                              mito.low = .02,
+                              mito.high = .975,
+                              feature.cut = .975){
   seurat.obj <- PercentageFeatureSet(seurat.obj, pattern = "^MT-", col.name = "percent.mt")
   ident <- data.table::setDT(Seurat::FetchData(seurat.obj, vars = c("orig.ident","percent.mt","nFeature_RNA")),
-                 keep.rownames = TRUE) 
+                             keep.rownames = TRUE) 
   ident.list <- as.list(unique(ident$orig.ident))
   mito.low <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$percent.mt,
-                                               probs = parameters[["mito.low"]]))
+                                               probs = mito.low))
   mito.high <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$percent.mt,
-                                               probs = parameters[["mito.high"]]))
+                                                probs = mito.high))
   feat.cut <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$nFeature_RNA,
-                                                probs = parameters[["feature.cut"]]))
+                                               probs = feature.cut))
   filter.list <- vector(mode = "list", length = length(ident.list))
   for (i in 1:length(ident.list)){ 
     ident.current <- ident[ident$orig.ident == ident.list[[i]]]
