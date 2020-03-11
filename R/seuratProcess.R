61efea24c98d95972b34825660743b76e78e3c01
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-11 11:20:09 -0500

    Create seuratProcess.R

---
+++ R/seuratProcess.R
@@ -0,0 +1,25 @@
+seuratProcess <- function(object = seurat.obj,
+                          dims = 1:50,
+                          cluster.res = 0.2,
+                          seed.use = 24,
+                          n.cores = NULL) {
+  
+  if(!is.null(n.cores)){
+    ifelse(n.cores > future::availableCores()[[1]], 
+           stop("n.cores is greater than available system cores"),
+           future::plan("multicore", workers = n.cores))
+  }
+  
+  seurat.obj <- seurat.obj %>%
+                Seurat::SCTransform(vars.to.regress = c("percent.mt","orig.ident"),
+                            verbose = FALSE) %>%
+                Seurat::RunPCA() %>%
+                Seurat::FindNeighbors(dims = dims) %>%
+                Seurat::RunUMAP(reduction = "pca", dims = dims, seed.use = seed.use)
+
+  return(seurat.obj)
+}
+                                     
+
+
+                          
