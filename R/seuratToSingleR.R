f378bcda17776c2f24794c9a19e2392461de9c7d
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-12 00:38:12 -0500

    Create seuratToSingleR.R

---
+++ R/seuratToSingleR.R
@@ -0,0 +1,9 @@
+#Produce SingleR annotations on a custom reference made from a Seurat object 
+
+seuratToSingleR <- function(object = seurat.obj,
+                            reference = reference.obj){ 
+                            
+                            
+                            
+                            
+                            } 
