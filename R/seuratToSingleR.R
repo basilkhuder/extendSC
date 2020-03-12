743c4d3a226a30029b45b81ea1b216abf1cb2139
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-12 00:43:26 -0500

    Update seuratToSingleR.R

--- R/seuratToSingleR.R
+++ R/seuratToSingleR.R
@@ -1,9 +1,19 @@
 #Produce SingleR annotations on a custom reference made from a Seurat object 
 
 seuratToSingleR <- function(object = seurat.obj,
-                            reference = reference.obj){ 
+                            reference = reference.obj,
+                            method = method){ 
+  
+  seurat_obj_avg <- Seurat::AverageExpression(reference_obj, assay = "RNA")
+  labels <- colnames(reference_obj_avg)
+  SingleR::SingleR(test = as.SingleCellExperiment(seurat_obj), 
+                   ref = seurat_obj_ag, labels = labels, 
+                   method = method)
+ 
+
+
                             
                             
                             
                             
-                            } 
+} 
