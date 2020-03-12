c2de8bb722fba460eb75797e655c46a85622c7b5
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-12 00:06:46 -0500

    Update umapCellAnno.R

--- R/umapCellAnno.R
+++ R/umapCellAnno.R
@@ -1,38 +1,68 @@
-# Takes a Seurat object and returns a UMAP embedding with cell counts as labels
-
 umapCellAnno <- function(seurat.obj,
-                     point.size = 1,
-                     label.size = 8,
-                     title = "",
-                     counts.as.title = FALSE,
-                     legend.pos = "right",
-                     use.colors = ""){  
+                         point.size = 1,
+                         label.size = 8,
+                         title = "",
+                         legend.title.size = 0,
+                         legend.text.size = 15,
+                         axis.title.x.size = 15,
+                         axis.title.y.size = 15, 
+                         axis.text.y.left.size = 15,
+                         axis.text.x.bottom.size = 15,
+                         counts.as.title = FALSE,
+                         counts.as.labels = FALSE,
+                         legend = TRUE,
+                         counts.in.legend = TRUE,
+                         use.cols = ""){
   
   `%>%` <- magrittr::`%>%`
-  extract.clusters <- data.table::setDT(Seurat::FetchData(seurat.obj, vars = c("seurat_clusters")), 
-                                        keep.rownames = TRUE)
+  if(!is.null(seurat.obj$Seurat_Assignment)) { 
+    vars <- "Seurat_Assignment"
+  } else { 
+    vars <- "seurat_clusters"
+  }
   
+  umap <- as.data.frame(Embeddings(seurat.obj, reduction = "umap")) %>%
+                        dplyr::mutate(Clusters = Seurat::FetchData(seurat.obj, vars = vars)[[1]]) %>%
+                        magrittr::set_rownames(NULL)
+  extract.clusters <- data.table::setDT(Seurat::FetchData(seurat.obj, vars = vars),keep.rownames = TRUE)
   cluster.counts <- extract.clusters %>%
-                    dplyr::group_by(seurat_clusters) %>%
-                    dplyr::tally() %>%
-                    dplyr::pull(n) %>%
-                    data.table::setattr("names", levels(seurat.obj))
-    
+                    dplyr::group_by_at(2) %>%
+                    dplyr::tally()
+  
+  if (use.cols == ""){
+    use.cols <- hcl(h = seq(15, 375, length = length(unique(extract.clusters[[2]])) + 1), 
+                    c = 100,
+                    l = 65)[1:length(unique(extract.clusters[[2]]))]
+  }
+  
   if (counts.as.title == TRUE){ 
     title = paste(scales::comma(sum(cluster.counts$n)),"Cells")
   }
   
-  
-  if (use.colors == ""){
-    use.colors <- hcl(h = seq(15, 375, length = length(unique(extract.clusters$seurat_clusters)) + 1), 
-                      c = 100,
-                      l = 65)[1:length(unique(extract.clusters$seurat_clusters))]
-  }
+  p1 <- ggplot2::ggplot(data = umap, mapping = aes(x = UMAP_1, y = UMAP_2))+
+    ggplot2::geom_point(aes(color = Clusters), size = .5) +
+    ggplot2::theme_bw() + 
+    ggplot2::theme(
+      plot.background = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_blank()) +
+    ggplot2::theme(axis.line = element_line(color = 'black'),
+                   legend.title = element_text(size = 0),
+                   legend.text = element_text(size = 15),
+                   axis.title.x = element_text(size = 15),
+                   axis.title.y = element_text(size = 15),
+                   axis.text.y.left = element_text(size = 15),
+                   axis.text.x.bottom = element_text(size = 15)) +
+    ggplot2::guides(colour = guide_legend(override.aes = list(size=10))) 
     
-  return(Seurat::DimPlot(object = Seurat::RenameIdents(object = seurat.obj, cluster.counts), 
-                reduction = 'umap', 
-                pt.size = point.size, 
-                label = TRUE, 
-                label.size = label.size,
-                cols = use.colors) + gplot2::gggtitle(title) + ggplot2::theme(legend.position = legend.pos))    
+    p1 <- Seurat::LabelClusters(p1, id = "Clusters", size = 10, repel = TRUE) + 
+    ggplot2::ggtitle(title)
+  
+  if (isTRUE(counts.in.legend)){ 
+    labels <- as.character(glue::glue("{cluster.counts[[1]]}\n ({cluster.counts[[2]]} Cells)"))
+    labels <- paste0(labels,"\n")
+    p1 <- p1 + ggplot2::scale_color_manual(values = use.cols, labels = labels)
+  }
+  return(p1)
 } 
