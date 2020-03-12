e906d9b06fd0ea70f349714ea8f1b72b0cb00709
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-03-12 01:07:30 -0500

    Update seuratToSingleR.R

--- R/seuratToSingleR.R
+++ R/seuratToSingleR.R
@@ -2,12 +2,13 @@
 
 seuratToSingleR <- function(object = seurat.obj,
                             reference = reference.obj,
-                            method = method){ 
+                            method = method,
+                            heatmap = TRUE){ 
   
-  seurat_obj_avg <- Seurat::AverageExpression(reference_obj, assay = "RNA")
-  labels <- colnames(reference_obj_avg)
-  SingleR::SingleR(test = as.SingleCellExperiment(seurat_obj), 
-                   ref = seurat_obj_ag, labels = labels, 
+  reference_avg <- as.matrix(Seurat::AverageExpression(reference_obj, assay = "RNA")$RNA)
+  labels <- colnames(reference_avg)
+  SR.output <- SingleR::SingleR(test = as.SingleCellExperiment(seurat_obj), 
+                   ref = reference_avg, labels = labels, 
                    method = method)
-                   
+  return(pheatmap::pheatmap(SR.output))
   } 
