e8092385b7bc8b289bfaa8255ac311a3ee0871ce
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-05 15:41:32 -0500

    Update renameClusters.R

--- R/renameClusters.R
+++ R/renameClusters.R
@@ -1,6 +1,6 @@
 renameClusters <- function(seurat.obj, cluster_names){ 
   seurat.obj.cluster.ids <- cluster_names
-  names(seurat.obj.cluster.ids) <- levels(x = seurat.obj)
+  names(seurat.obj.cluster.ids) <- levels(seurat.obj)
   seurat.obj <- RenameIdents(seurat.obj, seurat.obj.cluster.ids)
   seurat.obj@meta.data$Seurat_Assignment <- Idents(seurat.obj)
   return(seurat.obj)
