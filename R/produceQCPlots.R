8f22fcfd67dbb81ade29b5bd84decff8d17dfb52
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-06 01:33:02 -0500

    Update produceQCPlots.R

--- R/produceQCPlots.R
+++ R/produceQCPlots.R
@@ -13,24 +13,20 @@ produceQCPlots <- function(ident,
                                                  )),
                                keep.rownames = TRUE)
   }
-  
   ident.names <- unique(ident$orig.ident)
   ident.list <- lapply(seq_along(ident.names), 
                        function(x) dplyr::filter(ident, orig.ident == ident.names[x]))
-  
   plot1 <- lapply(seq_along(ident.list), function(x)
     ggplot(data = ident.list[[x]], mapping = aes(x = nCount_RNA, y = percent.mt)) +
     geom_point(color = "#f8766d") + 
     geom_hline(yintercept=mito.low[[x]], linetype="dashed", color = "red") +
     geom_hline(yintercept=mito.high[[x]], linetype="dashed", color = "red") +
     ggtitle(ident.names[[x]]))
-  
   plot2 <- lapply(seq_along(ident.list), function(x)
     ggplot(data = ident.list[[x]], mapping = aes(x = nCount_RNA, y = nFeature_RNA)) +
       geom_point(color = "#f8766d") + 
       geom_hline(yintercept=feat.cut[[x]], linetype="dashed", color = "red") +
       ggtitle(ident.names[[x]]))
-  
   print(plot1)
   print(plot2)
 }
