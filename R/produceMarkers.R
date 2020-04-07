8a9f61cbdcb65ed493c49fe3d0f2a23cfa8f6929
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-07 17:02:29 -0500

    Update produceMarkers.R

--- R/produceMarkers.R
+++ R/produceMarkers.R
@@ -1,14 +1,18 @@
 produceMarkers <- function(seurat.obj,
                               cells.per.ident = Inf,
                               top.gene.plot = TRUE){ 
-  
+  `%>%` <- magrittr::`%>%`
   date <- gsub("-","",Sys.Date())
-  markers <- FindAllMarkers(object = seurat.obj, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, max.cells.per.ident = cells.per.ident)
+  markers <- Seurat::FindAllMarkers(object = seurat.obj, 
+                            only.pos = TRUE, 
+                            min.pct = 0.25, 
+                            logfc.threshold = 0.25, 
+                            max.cells.per.ident = cells.per.ident)
   markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
   write.table(markers %>% group_by(cluster), file= paste0(date,"markers.txt"), row.names=FALSE, sep="\t")
   if(top.gene.plot == TRUE){
-    cluster.averages <- AverageExpression(object = seurat.obj, return.seurat = TRUE)
+    cluster.averages <- Seurat::AverageExpression(object = seurat.obj, return.seurat = TRUE)
     top3 <- markers %>% group_by(cluster) %>% top_n(n = 3, wt = avg_logFC)
-    print(pheatmap(GetAssayData(cluster.averages)[top3$gene,]))
+    print(pheatmap::pheatmap(GetAssayData(cluster.averages)[top3$gene,]))
     }
   }
