09da714114e389b37d03ef0c2c2749f66a507973
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-08 07:37:27 -0500

    Update mergeSubCluster.R

--- R/mergeSubCluster.R
+++ R/mergeSubCluster.R
@@ -1,7 +1,7 @@
 mergeSubCluster <- function(seurat.obj, subcluster.obj, cluster_replace){ 
-  seurat.obj.ident <- WhichCells(object = seurat.obj, ident = cluster_replace)
-  seurat.obj <- SetIdent(object = seurat.obj, cells = seurat.obj.ident,
-                         value = Idents(object = subcluster.obj))
+  seurat.obj.ident <- Seurat::WhichCells(object = seurat.obj, ident = cluster_replace)
+  seurat.obj <- Seurat::SetIdent(object = seurat.obj, cells = seurat.obj.ident,
+                         value = Seurat::Idents(object = subcluster.obj))
   seurat.obj@meta.data$Seurat_Assignment <- Idents(object = seurat.obj)
   return(seurat.obj)
 }
