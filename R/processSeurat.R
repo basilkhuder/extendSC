d1890aadb2d13c4a93d1d47b355ed12924201004
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-14 09:40:43 -0500

    Update processSeurat.R

--- R/processSeurat.R
+++ R/processSeurat.R
@@ -1,5 +1,3 @@
-#For multiple cluster resolutions, use a list where first element is "from", second is "to", "third" is by
-
 processSeurat <- function(seurat.obj,
                           dims = 1:50,
                           cluster.res = 0.2,
@@ -13,19 +11,17 @@ processSeurat <- function(seurat.obj,
            future::plan("multicore", workers = n.cores))
   }
   seurat.obj <- seurat.obj %>%
-                SCTransform(vars.to.regress = vars_to_regress,
-                            verbose = FALSE) %>%
-                RunPCA() %>%
-                FindNeighbors(dims = dims) 
+    SCTransform(vars.to.regress = vars_to_regress,
+                verbose = FALSE) %>%
+    RunPCA() %>%
+    FindNeighbors(dims = dims) 
   if(class(cluster.res) == "list"){ 
     res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
-    seurat.list <- vector(mode = "list",length = length(res.range))
     seurat.list <- lapply(seq_along(seurat.list), function(x)  
-      FindClusters(seurat.obj, resolution = res.range[[x]]) )
+      FindClusters(seurat.obj, resolution = res.range[[x]]))
     seurat.list <- lapply(seq_along(seurat.list), function(x)  
       RunUMAP(seurat.list[[x]], reduction = "pca", dims = dims, seed.use = seed.use)) 
-    seurat.obj <- seurat.list
-    } else { 
+  } else { 
     seurat.obj <- FindClusters(seurat.obj, resolution = cluster.res)
     seurat.obj <- RunUMAP(seurat.obj, reduction = "pca", dims = dims, seed.use = seed.use)
   }
