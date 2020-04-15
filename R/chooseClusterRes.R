2be7b38fb1e097a30801a708875b964d0bebe727
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-14 23:35:07 -0500

    Update chooseClusterRes.R

--- R/chooseClusterRes.R
+++ R/chooseClusterRes.R
@@ -1,7 +1,13 @@
-chooseClusterRes <- function(seurat.obj.list){ 
-
-  umap.list <- lapply(seq_along(seurat.obj.list), function(x) umapCellAnno(seurat.obj.list[[x]]) 
-
-
-
+chooseClusterRes <- function(seurat.obj, res.range){
+  umap <- try(Embeddings(seurat.obj, reduction = "umap"))
+  if(class(umap) == "try-error"){ 
+    stop("No UMAP coordinates. Run the RunUMAP() function")
+    } 
+    res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
+    cluster.list <- lapply(seq_along(res.range), function(x)
+        Idents(FindClusters(seurat.obj, 
+                            resolution = res.range[[x]], 
+                            verbose = FALSE)))
+    return(clusterPlots(umap, cluster.list))
 } 
+
