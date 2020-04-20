25ad13056b0150c654d8e3cef3cff80be6ab6921
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-20 10:14:58 -0500

    Update chooseClusterRes.R

--- R/chooseClusterRes.R
+++ R/chooseClusterRes.R
@@ -1,13 +1,23 @@
-chooseClusterRes <- function(seurat.obj, res.range){
-  umap <- try(Embeddings(seurat.obj, reduction = "umap"))
-  if(class(umap) == "try-error"){ 
-    stop("No UMAP coordinates. Run the RunUMAP() function")
+chooseClusterRes <- function(seurat.obj, cluster.res){
+    umap <- try(Embeddings(seurat.obj, reduction = "umap"))
+    if(class(umap) == "try-error"){ 
+        stop("No UMAP coordinates. Run the RunUMAP() function")
     } 
     res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
     cluster.list <- lapply(seq_along(res.range), function(x)
         Idents(FindClusters(seurat.obj, 
                             resolution = res.range[[x]], 
                             verbose = FALSE)))
-    return(clusterPlots(umap, cluster.list))
+    cluster.list <- lapply(seq_along(cluster.list), function(x)
+      as.data.frame(cbind(umap, cluster.list[[x]])))
+    
+    cluster.counts <- vector(mode = "list", length = length(cluster.list))
+    for(i in 1:length(cluster.counts)){
+      cluster.counts[[i]] <- cluster.list[[i]] %>%
+      dplyr::group_by_at(3) %>%
+      dplyr::tally() %>%
+      dplyr::arrange(desc(n))
+      cluster.list[[i]]$V3 <- factor(cluster.list[[i]]$V3 , levels = cluster.counts[[i]][[1]])
+      cluster.list[[i]] <- cluster.list[[i]][order(cluster.list[[i]]$V3), ]
+    }
 } 
-
