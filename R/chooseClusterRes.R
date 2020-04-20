5fbb3bc3df0c48fe1af34f18f76ac0d1fc9057c0
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-20 13:29:19 -0500

    Update chooseClusterRes.R

--- R/chooseClusterRes.R
+++ R/chooseClusterRes.R
@@ -1,27 +1,28 @@
 chooseClusterRes <- function(seurat.obj, cluster.res){
-    `%>%` <- magrittr::`%>%`
-    umap <- try(Embeddings(seurat.obj, reduction = "umap"))
+    umap <- try(as.data.frame(Embeddings(seurat.obj, reduction = "umap")))
     if(class(umap) == "try-error"){ 
         stop("No UMAP coordinates. Run the RunUMAP() function")
     } 
+    umap <- data.table::setDT(umap, keep.rownames = TRUE)
     res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
     cluster.list <- lapply(seq_along(res.range), function(x)
-        Seurat::Idents(Seurat::FindClusters(seurat.obj, 
+        tibble::enframe(Idents(FindClusters(seurat.obj, 
                             resolution = res.range[[x]], 
-                            verbose = FALSE)))
+                            verbose = FALSE)), name = "rn", value = "Clusters"))
+    
     cluster.list <- lapply(seq_along(cluster.list), function(x)
-      as.data.frame(cbind(umap, cluster.list[[x]])))
+      dplyr::full_join(umap, cluster.list[[x]]))
     
     cluster.counts <- vector(mode = "list", length = length(cluster.list))
     for(i in 1:length(cluster.counts)){
       cluster.counts[[i]] <- cluster.list[[i]] %>%
-      dplyr::group_by_at(3) %>%
+      dplyr::group_by_at(4) %>%
       dplyr::tally() %>%
       dplyr::arrange(desc(n))
-      cluster.list[[i]]$V3 <- factor(cluster.list[[i]]$V3 , levels = cluster.counts[[i]][[1]])
-      cluster.list[[i]] <- cluster.list[[i]][order(cluster.list[[i]]$V3), ]
+      cluster.list[[i]]$Clusters <- factor(cluster.list[[i]]$Clusters , 
+                                           levels = cluster.counts[[i]][[1]])
+      cluster.list[[i]] <- cluster.list[[i]][order(cluster.list[[i]]$Clusters), ]
     }
-    
     clusterPlots(cluster.list = cluster.list,
                  cluster.res = res.range)
 } 
