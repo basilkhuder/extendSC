333f01e0d6dd83c9eca08652d88345b4af433447
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-21 02:01:37 -0500

    Update clusterPlots.R

--- R/clusterPlots.R
+++ R/clusterPlots.R
@@ -1,4 +1,5 @@
-clusterPlots <- function(cluster.list,
+clusterPlots <- function(seurat.obj,
+                         cluster.list,
                          cluster.res,
                          feature.plot,
                          plot.cols,
@@ -14,44 +15,48 @@ clusterPlots <- function(cluster.list,
                          legend = TRUE,
                          cell.legend.size = 5, 
                          counts.in.legend = TRUE){
+  
+  `%>%` <- magrittr::`%>%`
+  for(i in 1:length(cluster.list)){ 
+    title <- glue::glue(cluster.res[[i]], " Cluster Resolution")
+    use.cols <- hcl(h = seq(15, 375, length = length(levels(cluster.list[[i]]$Clusters)) + 1), 
+                    c = 100,
+                    l = 65)[1:length(levels(cluster.list[[i]]$Clusters))]
+    p1 <- ggplot2::ggplot(data = cluster.list[[i]], 
+                          mapping = aes(x = UMAP_1, y = UMAP_2)) +
+      ggplot2::geom_point(aes(color = Clusters), size = point.size) +
+      scale_color_manual(values = use.cols) + 
+      ggplot2::theme_bw() + 
+      ggplot2::theme(
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank()) +
+      ggplot2::theme(axis.line = element_line(color = 'black'),
+                     legend.title = element_text(size = legend.title.size),
+                     legend.text = element_text(size = legend.text.size),
+                     axis.title.x = element_text(size = axis.title.x.size),
+                     axis.title.y = element_text(size = axis.title.y.size),
+                     axis.text.y.left = element_text(size = axis.text.y.left.size),
+                     axis.text.x.bottom = element_text(size = axis.text.x.bottom.size)) +
+      ggplot2::guides(colour = guide_legend(override.aes = list(size=cell.legend.size))) 
     
-    `%>%` <- magrittr::`%>%`
-    for(i in 1:length(cluster.list)){ 
-        title <- glue::glue(cluster.res[[i]], " Cluster Resolution")
-        use.cols <- hcl(h = seq(15, 375, length = length(levels(cluster.list[[i]]$Clusters)) + 1), 
-                        c = 100,
-                        l = 65)[1:length(levels(cluster.list[[i]]$Clusters))]
-        p1 <- ggplot2::ggplot(data = cluster.list[[i]], 
-                              mapping = aes(x = UMAP_1, y = UMAP_2)) +
-            ggplot2::geom_point(aes(color = Clusters), size = point.size) +
-            scale_color_manual(values = use.cols) + 
-            ggplot2::theme_bw() + 
-            ggplot2::theme(
-                plot.background = element_blank(),
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.border = element_blank()) +
-            ggplot2::theme(axis.line = element_line(color = 'black'),
-                           legend.title = element_text(size = legend.title.size),
-                           legend.text = element_text(size = legend.text.size),
-                           axis.title.x = element_text(size = axis.title.x.size),
-                           axis.title.y = element_text(size = axis.title.y.size),
-                           axis.text.y.left = element_text(size = axis.text.y.left.size),
-                           axis.text.x.bottom = element_text(size = axis.text.x.bottom.size)) +
-            ggplot2::guides(colour = guide_legend(override.aes = list(size=cell.legend.size))) 
-        
-        print(Seurat::LabelClusters(p1, id = "Clusters", size = label.size, repel = TRUE) + 
-                   ggplot2::ggtitle(title))
-        
-        new.ident <- cluster.list[[i]]$Clusters
-        names(new.ident) <- cluster.list[[i]]$rn
-        Idents(seurat.obj) <- new.ident
-        produceMarkers(seurat.obj, cells.per.ident = 50, top.gene.plot = TRUE)
-        if(!is.null(feature.plot)){ 
-          FeaturePlot(seurat.obj, cols = c("grey", "red"),
-                      features = feature.plot,
-                      reduction = "umap", ncol = plot.cols)
-          }
-        
-    } 
+    print(Seurat::LabelClusters(p1, id = "Clusters", size = label.size, repel = TRUE) + 
+            ggplot2::ggtitle(title))
+    
+    new.ident <- cluster.list[[i]]$Clusters
+    names(new.ident) <- cluster.list[[i]]$rn
+    Idents(seurat.obj) <- new.ident
+    
+    produceMarkers(seurat.obj,
+                   cells.per.ident = 50, 
+                   top.gene.plot = TRUE)
+    
+    if(!is.null(feature.plot)){ 
+      print(FeaturePlot(seurat.obj, cols = c("grey", "red"),
+                  features = feature.plot,
+                  reduction = "umap", ncol = plot.cols))
+    }
+    
+  } 
 } 
