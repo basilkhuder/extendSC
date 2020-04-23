f15f227d13587f1af2b4154b4d0347e768f42977
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-23 09:42:05 -0500

    Update chooseClusterRes.R

--- R/chooseClusterRes.R
+++ R/chooseClusterRes.R
@@ -1,6 +1,7 @@
 chooseClusterRes <- function(seurat.obj, 
                              cluster.res,
-                             feature.plot = NULL,
+                             ident.plot = TRUE,
+                             feature.plot = TRUE,
                              plot.cols = NULL){
   
   `%>%` <- magrittr::`%>%`
@@ -20,20 +21,18 @@ chooseClusterRes <- function(seurat.obj,
   cluster.list <- lapply(seq_along(res.range), function(x)
     dplyr::full_join(umap, cluster.list[[x]]))
   
-  #cluster.counts <- vector(mode = "list", length = length(cluster.list))
-  #for(i in 1:length(cluster.counts)){
-  #  cluster.counts[[i]] <- cluster.list[[i]] %>%
-  #    dplyr::group_by_at(4) %>%
-  #    dplyr::tally() %>%
-  #    dplyr::arrange(desc(n))
-  #  cluster.list[[i]]$Clusters <- factor(cluster.list[[i]]$Clusters , 
-  #                                       levels = cluster.counts[[i]][[1]])
-  #  cluster.list[[i]] <- cluster.list[[i]][order(cluster.list[[i]]$Clusters), ]
-  #}
+  cluster.counts <- vector(mode = "list", length = length(cluster.list))
+  
+  # for(i in 1:length(cluster.list)){
+  #   cluster.list[[i]]$Clusters <- factor(cluster.list[[i]]$Clusters, 
+  #                                        levels = unique(cluster.list[[i]]$Clusters))
+  #   cluster.list[[i]] <- cluster.list[[i]][order(cluster.list[[i]]$Clusters), ]
+  # }
   
   clusterPlots(seurat.obj = seurat.obj,
                cluster.list = cluster.list,
                cluster.res = res.range,
                feature.plot = feature.plot,
-               plot.cols = plot.cols)
+               plot.cols = plot.cols,
+               ident.plot = ident.plot)
 } 
