79cdda983541a1d47183f83ff51460a83c191452
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-24 13:37:29 -0500

    Update plotModuleScores.R

--- R/plotModuleScores.R
+++ R/plotModuleScores.R
@@ -3,19 +3,16 @@
 plotModuleScores <- function(seurat.obj,
                              features,
                              name = NULL){ 
-                             
-if(!is.null(name)){ 
-  name <- name
-  } else { 
-  name <- "Module_scores"
-  } 
   
+  if(!is.null(name)){ 
+    name <- name
+  } else { 
+    name <- "Module_scores"
+  }
   m.obj <- Seurat::AddModuleScore(object = seurat.obj,
-                        features = features,
-                        name = "Module_Scores")
-                        
- print(Seurat::RidgePlot(m.obj, features = name) + ggplot2::ggtitle(name))
- print(Seurat::FeaturePlot(m.obj, features = name) + ggplot2::ggtitle(name)) 
- 
+                                  features = features,
+                                  name = "Module_Scores")
+  print(Seurat::RidgePlot(m.obj, features = name) + ggplot2::ggtitle(name))
+  print(Seurat::FeaturePlot(m.obj, features = name) + ggplot2::ggtitle(name)) 
  } 
                              
