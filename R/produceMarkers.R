ecec917a2845f934bcab54cdfd59927b4df0452c
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-24 12:38:49 -0500

    Update produceMarkers.R

--- R/produceMarkers.R
+++ R/produceMarkers.R
@@ -1,17 +1,23 @@
 #' @export
 produceMarkers <- function(seurat.obj,
-                              cells.per.ident = Inf,
-                              top.gene.plot = TRUE){ 
+                           cells.per.ident = Inf,
+                           top.gene.plot = TRUE,
+                           file.name = NULL ){ 
   `%>%` <- magrittr::`%>%`
-  date <- gsub("-","",Sys.Date())
   markers <- Seurat::FindAllMarkers(object = seurat.obj, 
                             only.pos = TRUE, 
                             min.pct = 0.25, 
                             logfc.threshold = 0.25, 
                             max.cells.per.ident = cells.per.ident)
   markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+  
+  if(!is.null(file.name)){ 
+    file.name <- file.name
+  } else { 
+    file.name <- paste0(gsub("-","",Sys.Date()),"markers.txt")
+  } 
   write.table(markers %>% group_by(cluster), 
-              file= paste0(date,"markers.txt"),
+              file = file.name),
               row.names=FALSE, sep="\t")
   if(top.gene.plot == TRUE){
     cluster.averages <- Seurat::AverageExpression(object = seurat.obj, return.seurat = TRUE)
