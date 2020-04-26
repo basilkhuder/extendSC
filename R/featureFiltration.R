f66a00f2690b7ab7f0b5c08df34344716abfc16a
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-26 11:16:44 -0500

    Update featureFiltration.R

--- R/featureFiltration.R
+++ R/featureFiltration.R
@@ -2,36 +2,45 @@ featureFiltration <- function(seurat.obj,
                               mito.low = .05,
                               mito.high = .975,
                               feature.cut = .975,
-                              produce_plots = TRUE ){
-  seurat.obj <- Seurat::PercentageFeatureSet(seurat.obj, 
-                                             pattern = "^MT-", 
+                              produce_plots = TRUE) {
+  seurat.obj <- Seurat::PercentageFeatureSet(seurat.obj,
+                                             pattern = "^MT-",
                                              col.name = "percent.mt")
-  ident <- data.table::setDT(Seurat::FetchData(seurat.obj, 
-                                               vars = c("orig.ident",
-                                                        "percent.mt",
-                                                        "nFeature_RNA",
-                                                        "nCount_RNA")),
-                             keep.rownames = TRUE) 
+  ident <- data.table::setDT(Seurat::FetchData(
+    seurat.obj,
+    vars = c("orig.ident",
+             "percent.mt",
+             "nFeature_RNA",
+             "nCount_RNA")
+  ),
+  keep.rownames = TRUE)
   
   ident.list <- as.list(unique(ident$orig.ident))
-  mito.low <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$percent.mt,
-                                               probs = mito.low))
-  mito.high <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$percent.mt,
-                                                probs = mito.high))
-  feat.cut <- purrr::map(ident.list, ~quantile(ident[ident$orig.ident == .x,]$nFeature_RNA,
-                                               probs = feature.cut))
+  mito.low <-
+    purrr::map(ident.list, ~ quantile(ident[ident$orig.ident == .x, ]$percent.mt,
+                                      probs = mito.low))
+  mito.high <-
+    purrr::map(ident.list, ~ quantile(ident[ident$orig.ident == .x, ]$percent.mt,
+                                      probs = mito.high))
+  feat.cut <-
+    purrr::map(ident.list,
+               ~ quantile(ident[ident$orig.ident == .x, ]$nFeature_RNA,
+                          probs = feature.cut))
   filter.list <- vector(mode = "list", length = length(ident.list))
   
-  if(produce_plots == TRUE){ 
+  if (produce_plots == TRUE) {
     produceQCPlots(ident, mito.low, mito.high, feat.cut)
-    }
+  }
   
-  for (i in 1:length(ident.list)){ 
+  for (i in 1:length(ident.list)) {
     ident.current <- ident[ident$orig.ident == ident.list[[i]]]
-    ident.current <- ident.current[ident.current$percent.mt > mito.low[[i]]]
-    ident.current <- ident.current[ident.current$percent.mt < mito.high[[i]]]
-    ident.current <- ident.current[ident.current$nFeature_RNA < feat.cut[[i]]]
+    ident.current <-
+      ident.current[ident.current$percent.mt > mito.low[[i]]]
+    ident.current <-
+      ident.current[ident.current$percent.mt < mito.high[[i]]]
+    ident.current <-
+      ident.current[ident.current$nFeature_RNA < feat.cut[[i]]]
     filter.list[[i]] <- ident.current
   }
-  return(subset(seurat.obj,cells = do.call(rbind, filter.list)$rn))
+  return(subset(seurat.obj, cells = do.call(rbind, filter.list)$rn))
 }
