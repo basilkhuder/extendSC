28a7c6a06e7bbd85616a0739a3fb296564ea1eac
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-26 23:04:21 -0500

    Update produceQCPlots.R

--- R/produceQCPlots.R
+++ R/produceQCPlots.R
@@ -3,15 +3,14 @@ produceQCPlots <- function(ident,
                            mito.high,
                            mito.low,
                            feat.cut) {
-    ident.names <- unique(ident$orig.ident)
-    ident.list <- lapply(seq_along(ident.names),
-                         function(x)
-                             dplyr::filter(ident, orig.ident == ident.names[x]))
     
-    for (i in 1:length(unique(ident$orig.ident)) {
-        plot1 <-
-            ggplot2::ggplot(data = ident.list[[i]],
-                            mapping = ggplot2::aes(x = nCount_RNA, y = percent.mt)) +
+    ident.names <- unique(ident$orig.ident)
+    ident.list <- lapply(seq_along(ident.names), function(x)
+        dplyr::filter(ident, orig.ident == ident.names[x]))
+    
+    for (i in 1:length(unique(ident$orig.ident))) { 
+        plot1 <- ggplot2::ggplot(data = ident.list[[i]],
+                                 mapping = ggplot2::aes(x = nCount_RNA, y = percent.mt)) +
             ggplot2::geom_point(color = "#f8766d") +
             ggplot2::geom_hline(yintercept = mito.low[[i]],
                                 linetype = "dashed",
@@ -19,16 +18,16 @@ produceQCPlots <- function(ident,
             ggplot2::geom_hline(yintercept = mito.high[[i]],
                                 linetype = "dashed",
                                 color = "red") +
-            ggplot2::ggtitle(ident.names[[i]]))
-    
+            ggplot2::ggtitle(ident.names[[i]])
+        
         plot2 <- ggplot2::ggplot(data = ident.list[[i]], 
                                  mapping = ggplot2::aes(x = nCount_RNA, y = nFeature_RNA)) +
             ggplot2::geom_point(color = "#f8766d") +
             ggplot2::geom_hline(yintercept = feat.cut[[i]], linetype = "dashed", color = "red") +
-            ggplot2::ggtitle(ident.names[[i]]))
+            ggplot2::ggtitle(ident.names[[i]])
+        
+        print(plot1)
+        print(plot2)
+        
+        } 
     }
-    
-    print(plot1)
-    print(plot2)
-}
-
