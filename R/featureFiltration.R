8551ef49f40e8ac8eddb73fd3fd169d055afed3d
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-27 01:40:18 -0500

    Create featureFiltration.R

---
+++ R/featureFiltration.R
@@ -0,0 +1,40 @@
+featureFiltration <- function(seurat.obj,
+                              mito.low = .05,
+                              mito.high = .975,
+                              feature.cut = .975,
+                              produce_plots = TRUE) {
+    seurat.obj <- Seurat::PercentageFeatureSet(seurat.obj,
+                                               pattern = "^MT-",
+                                               col.name = "percent.mt")
+    ident <- Seurat::FetchData(seurat.obj,
+                               vars = c("orig.ident",
+                                        "percent.mt",
+                                        "nFeature_RNA",
+                                        "nCount_RNA"))
+
+    
+    ident.list <- as.list(unique(ident$orig.ident))
+    mito.low <- lapply(ident.list, function(x) 
+        quantile(ident[ident$orig.ident == x, ]$percent.mt, probs = mito.low))
+    
+    mito.high <- lapply(ident.list, function(x)
+        quantile(ident[ident$orig.ident == x, ]$percent.mt, probs = mito.high))
+    
+    feat.cut <- lapply(ident.list, function(x)
+        quantile(ident[ident$orig.ident == x, ]$nFeature_RNA,
+                 probs = feature.cut))
+    
+    if (isTRUE(produce_plots)) {
+        produceQCPlots(ident, mito.low, mito.high, feat.cut)
+    }
+    
+    filter.list <- vector(mode = "list", length = length(ident.list))
+    for (i in 1:length(ident.list)) {
+        ident.current <- ident[ident$orig.ident == ident.list[[i]],]
+        ident.current <- ident.current[ident.current$percent.mt > mito.low[[i]],]
+        ident.current <- ident.current[ident.current$percent.mt < mito.high[[i]],]
+        ident.current <- ident.current[ident.current$nFeature_RNA < feat.cut[[i]],]
+        filter.list[[i]] <- ident.current
+    }
+    return(subset(seurat.obj, cells = rownames(do.call(rbind, filter.list))))
+}
