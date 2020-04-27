6eaa8dffc1758bd81c48a809ebbca43c9b86f20c
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-04-26 22:43:14 -0500

    Update produceQCPlots.R

--- R/produceQCPlots.R
+++ R/produceQCPlots.R
@@ -2,22 +2,33 @@
 produceQCPlots <- function(ident,
                            mito.high,
                            mito.low,
-                           feat.cut){ 
-    
+                           feat.cut) {
     ident.names <- unique(ident$orig.ident)
-    ident.list <- lapply(seq_along(ident.names), 
-                         function(x) dplyr::filter(ident, orig.ident == ident.names[x]))
-    plot1 <- lapply(seq_along(ident.list), function(x)
-        ggplot2::ggplot(data = ident.list[[x]], mapping = ggplot2::aes(x = nCount_RNA, y = percent.mt)) +
-            ggplot2::geom_point(color = "#f8766d") + 
-            ggplot2::geom_hline(yintercept=mito.low[[x]], linetype="dashed", color = "red") +
-            ggplot2::geom_hline(yintercept=mito.high[[x]], linetype="dashed", color = "red") +
-            ggplot2::ggtitle(ident.names[[x]]))
-    plot2 <- lapply(seq_along(ident.list), function(x)
-        ggplot2::ggplot(data = ident.list[[x]], mapping = ggplot2::aes(x = nCount_RNA, y = nFeature_RNA)) +
-            ggplot2::geom_point(color = "#f8766d") + 
-            ggplot2::geom_hline(yintercept=feat.cut[[x]], linetype="dashed", color = "red") +
-            ggplot2::ggtitle(ident.names[[x]]))
+    ident.list <- lapply(seq_along(ident.names),
+                         function(x)
+                             dplyr::filter(ident, orig.ident == ident.names[x]))
+    
+    for (i in 1:length(unique(ident$orig.ident)) {
+        plot1 <-
+            ggplot2::ggplot(data = ident.list[[i]],
+                            mapping = ggplot2::aes(x = nCount_RNA, y = percent.mt)) +
+            ggplot2::geom_point(color = "#f8766d") +
+            ggplot2::geom_hline(yintercept = mito.low[[i]],
+                                linetype = "dashed",
+                                color = "red") +
+            ggplot2::geom_hline(yintercept = mito.high[[i]],
+                                linetype = "dashed",
+                                color = "red") +
+            ggplot2::ggtitle(ident.names[[i]]))
+    
+        plot2 <- ggplot2::ggplot(data = ident.list[[i]], 
+                                 mapping = ggplot2::aes(x = nCount_RNA, y = nFeature_RNA)) +
+            ggplot2::geom_point(color = "#f8766d") +
+            ggplot2::geom_hline(yintercept = feat.cut[[i]], linetype = "dashed", color = "red") +
+            ggplot2::ggtitle(ident.names[[i]]))
+    }
+    
     print(plot1)
     print(plot2)
 }
+
