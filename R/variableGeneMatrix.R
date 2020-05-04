30d1e71ad15e0d0e59b0bb101612e9c4512db273
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-05-04 18:41:56 -0500

    Update variableGeneMatrix.R

--- R/variableGeneMatrix.R
+++ R/variableGeneMatrix.R
@@ -1,4 +1,14 @@
 #' @export
+#' Produces a variable gene correlation heatmap and returns a correlation matrix. 
+#' @param seurat.obj A seurat object. 
+#' @param variable.genes The amount of variables genes to include. 
+#' @param downsample Amount of cells to optionally downsample. 
+#' @param return.table Optional argument to return the correlation matrix
+#' @param fontsize The font size for the gene names on the heatmap. 
+#' @return A correlation heatmap and (optionally) a correlation matrix. 
+#' @examples
+#' variableGeneMatrix(seurat.obj = seurat.obj, variable.genes = 200, downsample = 1000, return.table = FALSE, fontsize = 10) 
+
 variableGeneMatrix <- function(seurat.obj,
                                variable.genes = 200,
                                downsample = NULL,
