7735b58d5140fbe1ad5794ba5c8844cabeab6301
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-05-04 16:52:32 -0500

    Update variableGeneMatrix.R

--- R/variableGeneMatrix.R
+++ R/variableGeneMatrix.R
@@ -1,17 +1,15 @@
 variableGeneMatrix <- function(seurat.obj,
-                            variable.genes = 200,
-                            downsample = NULL) { 
-seurat.obj <- Seurat::FindVariableFeatures(seurat.obj, nfeatures = variable.genes)
-features <- Seurat::VariableFeatures(seurat.obj)
-
-if(!is.null(downsample)){ 
-  seurat.obj = subset(seurat.obj, cells = sample(Seurat::Cells(seurat.obj), downsample))
+                               variable.genes = 200,
+                               downsample = NULL) { 
+  seurat.obj <- Seurat::FindVariableFeatures(seurat.obj, nfeatures = variable.genes)
+  features <- Seurat::VariableFeatures(seurat.obj)
+  
+  if(!is.null(downsample)){ 
+    seurat.obj = subset(seurat.obj, cells = sample(Seurat::Cells(seurat.obj), downsample))
   }
-
-expr.matrix <- GetAssayData(seurat.obj)[features,]
-gene.cor <- cor(t(gene.expr))
-print(pheatmap::pheatmap(gene.cor))
-
+  expr.matrix <- GetAssayData(seurat.obj)[features,]
+  gene.cor <- cor(t(gene.expr))
+  print(pheatmap::pheatmap(gene.cor))
 } 
 
 
