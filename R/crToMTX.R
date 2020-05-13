161246d3f26f922d9fe2dbad80d9f7191ee02114
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-05-13 13:27:45 -0500

    Update crToMTX.R

--- R/crToMTX.R
+++ R/crToMTX.R
@@ -1,15 +1,20 @@
 #' @export
 
 crToMTX <- function(folders) {
-  
   path <- paste(folders, list.files(path = folders,
                                     pattern = "filtered"), sep = "/")
   mtx <- paste(path, list.files(path = path,
                                 pattern = "matrix"), sep = "/")
   mtx <- Matrix::readMM(mtx)
-  
-  features <- paste(path, list.files(path = path,
-                                     pattern = "features"), sep = "/")
+  features <- list.files(path = path, pattern = "features")
+  if(isTRUE(identical(features, character(0)))){ 
+    features <- list.files(path = path, pattern = "gene")
+  }
+  if(isTRUE(identical(features, character(0)))){ 
+    stop("Features/Gene file not found. If file exists, rename as 'features.tsv' or 
+         'genes.tsv'")
+  }
+  features <- paste(path, features, sep = "/")
   features <- read.delim(features, header = FALSE, stringsAsFactors = FALSE)$V2
   barcodes <- paste(path, list.files(path = path,
                                      pattern = "barcodes"), sep = "/")
