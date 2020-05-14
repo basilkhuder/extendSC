fb9cc1684ebd48f9f8be1a9044920dcd085c5c8d
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-05-14 14:29:54 -0500

    Update variableGeneMatrix.R

--- R/variableGeneMatrix.R
+++ R/variableGeneMatrix.R
@@ -11,16 +11,18 @@
 
 variableGeneMatrix <- function(seurat.obj,
                                variable.genes = 200,
-                               downsample = NULL,
+                               #downsample = NULL,
                                return.table = FALSE,
                                fontsize = 10) { 
   
   seurat.obj <- Seurat::FindVariableFeatures(seurat.obj, nfeatures = variable.genes)
   features <- Seurat::VariableFeatures(seurat.obj)
   
-  if(!is.null(downsample)){ 
-    seurat.obj = subset(seurat.obj, cells = sample(Seurat::Cells(seurat.obj), downsample))
-  }
+  ### Downsampling is temp turned off. In next version, sampling will be stratified. 
+  #if(!is.null(downsample)){ 
+  #  seurat.obj = subset(seurat.obj, cells = sample(Seurat::Cells(seurat.obj), downsample))
+  #}
+  
   expr.matrix <- Matrix::t(GetAssayData(seurat.obj)[features,])
   gene.cor <- cor(as.matrix(expr.matrix))
   print(pheatmap::pheatmap(gene.cor, 
