7824c28119d6760585743ca472622443ac0383f6
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-03 15:21:29 -0400

    Update chooseClusterRes.R

--- R/chooseClusterRes.R
+++ R/chooseClusterRes.R
@@ -8,27 +8,28 @@
 #' @examples
 #' chooseClusterRes(seurat.obj, cluster.res = list(.1,.2,.5), feature.plot = c("gene1","gene2","gene3"))
 #' @export
+
 chooseClusterRes <- function(seurat.obj, 
                              cluster.res,
                              ident.plot = TRUE,
                              feature.plot = NULL,
                              plot.cols = NULL){
   
-  umap <- try(as.data.frame(Seurat::Embeddings(seurat.obj, 
-                                               reduction = "umap")))
+  umap <- try(as_tibble(Embeddings(seurat.obj,reduction = "umap"), 
+                        rownames = "Cells"), silent = TRUE)
+  
   if(class(umap) == "try-error"){ 
     stop("No UMAP coordinates. Run the RunUMAP() function")
   } 
-  umap <- data.table::setDT(umap, keep.rownames = TRUE)
-  res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
   
-  cluster.list <- lapply(seq_along(res.range), function(x)
-    tibble::enframe(Seurat::Idents(Seurat::FindClusters(seurat.obj, 
-                                                        resolution = res.range[[x]], 
-                                                        verbose = FALSE)),
-                    name = "rn", value = "Clusters"))
-  cluster.list <- lapply(seq_along(res.range), function(x)
-    dplyr::full_join(umap, cluster.list[[x]])) 
+  res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
+  cluster.list <- map(res.range, ~ Idents(FindClusters(seurat.obj, 
+                                                        resolution = res.range[[.x]], 
+                                                        verbose = FALSE)))
+  
+  cluster.list <- map(cluster.list, ~tibble(Cells = names(.x), 
+                                            Cluster = .x)) %>%
+    map( ~full_join(umap, .x))
   
   clusterPlots(seurat.obj = seurat.obj,
                cluster.list = cluster.list,
