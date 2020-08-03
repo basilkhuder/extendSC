2ef3c19ff5afedd35c5ca5cf86d48e1fdb504dd4
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-03 12:33:51 -0400

    Update processSeurat.R

--- R/processSeurat.R
+++ R/processSeurat.R
@@ -1,30 +1,32 @@
+#' Pipes together several downstream Seurat steps including variance-stabilizing transformation, PCA, clustering and nonlinear dimensionality reduction 
+#' @param seurat.obj A seurat object. 
+#' @param dims Dimensions of reduction to use as input for FindNeighbors and RunUMAP
+#' @param vars_to_regress Variables to regress out in a second non-regularized linear regression for SCTransform
+#' @param cluster.res Cluster resolution for FindClusters
+#' @param seed.use Seed to set for RunUMAP
+#' @param n.cores Number of cores to use
+#' @return A processed Seurat object 
+#' @examples
+#' processSeurat(seurat.obj, dims = 1:50, cluster.res = .2, vars_to_regress = c("orig.ident","percent.mt"))
 #' @export
+
 processSeurat <- function(seurat.obj,
                           dims = 1:50,
                           cluster.res = 0.2,
                           vars_to_regress = c("orig.ident","percent.mt"),
                           seed.use = 24,
                           n.cores = NULL) {
-  `%>%` <- magrittr::`%>%`
   if(!is.null(n.cores)){
-    ifelse(n.cores > future::availableCores()[[1]], 
-           stop("n.cores is greater than available system cores"),
-           future::plan("multicore", workers = n.cores))
+    if_else(n.cores > availableCores()[[1]], 
+            stop("n.cores is greater than available system cores"),
+            plan("multicore", workers = n.cores))
   }
-  seurat.obj <- seurat.obj %>%
-    Seurat::SCTransform(vars.to.regress = vars_to_regress,
-                verbose = FALSE) %>%
-    Seurat::RunPCA() %>%
-    Seurat::FindNeighbors(dims = dims) 
-  if(class(cluster.res) == "list"){ 
-    res.range <- seq(cluster.res[[1]], cluster.res[[2]], cluster.res[[3]])
-    seurat.list <- lapply(seq_along(res.range), function(x)  
-      Seurat::FindClusters(seurat.obj, resolution = res.range[[x]]))
-    seurat.list <- lapply(seq_along(seurat.list), function(x)  
-      Seurat::RunUMAP(seurat.list[[x]], reduction = "pca", dims = dims, seed.use = seed.use)) 
-  } else { 
-    seurat.obj <- Seurat::FindClusters(seurat.obj, resolution = cluster.res)
-    seurat.obj <- Seurat::RunUMAP(seurat.obj, reduction = "pca", dims = dims, seed.use = seed.use)
-  }
+  
+  seurat.obj <- seurat.obj %>% SCTransform(vars.to.regress = vars_to_regress, 
+                                           verbose = FALSE) %>%
+    RunPCA() %>% FindNeighbors(dims = dims) 
+    seurat.obj <- FindClusters(seurat.obj, resolution = cluster.res)
+    seurat.obj <- RunUMAP(seurat.obj, reduction = "pca", dims = dims, seed.use = seed.use)
+  
   return(seurat.obj)
 }
