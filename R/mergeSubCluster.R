acc5f375e212eb05e00987c6e457b13d9a5ddbb8
Basil Khuder <basilkhuder@Basils-MacBook-Pro.local>
2020-08-05 00:26:05 -0500

    Added annotation.name parameter

--- R/mergeSubCluster.R
+++ R/mergeSubCluster.R
@@ -1,9 +1,25 @@
+#' Merge a reclusted sub-sample back into main object
+#' @param seurat.obj A Seurat object. 
+#' @param subcluster.obj A Seurat sub-cluster object
+#' @param cluster.repace Clusters to replace
+#' @param annotation.name Name to store cluster annotations
+#' @return UMAP and a Feature Plot
+#' @examples
+#' chooseClusterRes(seurat.obj, cluster.res = list(.1,.2,.5), feature.plot = c("gene1","gene2","gene3"))
 #' @export
-mergeSubCluster <- function(seurat.obj, subcluster.obj, cluster_replace){ 
-  seurat.obj.ident <- Seurat::WhichCells(object = seurat.obj, ident = cluster_replace)
-  seurat.obj <- Seurat::SetIdent(object = seurat.obj, cells = seurat.obj.ident,
-                         value = Seurat::Idents(object = subcluster.obj))
-  seurat.obj@meta.data$Seurat_Assignment <- Idents(object = seurat.obj)
+#' @export
+mergeSubCluster <- function(seurat.obj, 
+                            subcluster.obj, 
+                            cluster.replace,
+                            annotation.name){ 
+  
+  if(is.null(annotation.name)) {
+    annotation.name <- "Seurat_Assignment"
+  }
+  seurat.obj.ident <- WhichCells(object = seurat.obj, ident = cluster.replace)
+  seurat.obj <- SetIdent(object = seurat.obj, cells = seurat.obj.ident,
+                         value = Idents(object = subcluster.obj))
+  seurat.obj@meta.data[[annotation.name]] <- Idents(object = seurat.obj)
   return(seurat.obj)
 }
   
