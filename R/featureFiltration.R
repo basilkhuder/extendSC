33750e337198b7d168474844c00e7db83ab99a5e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-07 23:30:10 -0500

    Update featureFiltration.R

--- R/featureFiltration.R
+++ R/featureFiltration.R
@@ -1,10 +1,16 @@
+#' Filters cells from a Seurat object based upon the amount of features and percentage of mitochondrial genes
+#' @param seurat.obj A seurat object. 
+#' @param mito.low Filter cells that have total mitochondrial genes low than this quantile
+#' @param mito.high Filter cells that have total mitochondrial genes higher than this quantile 
+#' @param feature.cut Filter cells that have total features higher than this quantile 
+#' @examples
+#' featureFiltration(seurat.obj, mito.low = .5, mito.high = .975, feature.cut = .975)
 #' @export
 
 featureFiltration <- function(seurat.obj,
                               mito.low = .05,
                               mito.high = .975,
-                              feature.cut = .975)
-                              #produce_plots = TRUE) {
+                              feature.cut = .975) {
   seurat.obj <- PercentageFeatureSet(seurat.obj,
                                      pattern = "^MT-",
                                      col.name = "percent.mt")
