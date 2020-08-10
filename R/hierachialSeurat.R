964686dfee1e2194903cc2cd11b3f4c1aec6af3e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-10 13:39:30 -0500

    Update hierachialSeurat.R

--- R/hierachialSeurat.R
+++ R/hierachialSeurat.R
@@ -4,7 +4,6 @@
 #' @param annotation.name Variable name given to Seurat cluster assignments 
 #' @param down.sample Amount of cells to sample 
 #' @param seed Value for the seed to set
-
 #' @param variable.genes (Optional) Subet counts data to this many variable genes for distance matrix calculation
 #' hierachialSeurat(seurat.obj, clusters = "Cluster1", annotation.name = "Seurat_Assignment", down.sample = 50) 
 #' @export
