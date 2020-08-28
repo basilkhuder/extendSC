7fec7da904220a1a25c066154f6823663a31d7dd
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-28 13:21:41 -0500

    Update summarizeModScore.R

--- R/summarizeModScore.R
+++ R/summarizeModScore.R
@@ -1,3 +1,20 @@
-summarizeModScore <- function(seurat.obj, features) { 
-  #TODO
-} 
+summarizeModScore <- function(seurat.obj,
+                              features,
+                              annotation.name) { 
+  
+  mod.scores <- extract2(AddModuleScore(object = bal.mp,features = list(features),
+                                        name = "module_score"),
+                         "module_score1") %>%
+  as_tibble(rownames = "Cells")
+  
+  clusters <- extract2(bal.mp, annotation.name) %>%
+    as_tibble(rownames = "Cells") %>%
+    full_join(mod.scores)
+  
+  print(clusters %>% 
+          group_by(!!as.name(annotation.name)) %>% 
+          rstatix::get_summary_stats())
+  } 
+
+
+
