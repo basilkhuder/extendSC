ee06da1f0842bfb8908ea8c845974e9bee829098
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-09-25 19:50:08 -0500

    Update crToSeurat.R

--- R/crToSeurat.R
+++ R/crToSeurat.R
@@ -2,6 +2,8 @@
 #' @param seurat.obj A Seurat object. 
 #' @param min_cells Include features detected in at least this many cells.
 #' @param min_features Include cells where at least this many features are detected 
+#' @param sample.names Names for each of the samples
+#' @param gz Whether or not the files are gunzipped
 #' @return A Seurat object 
 #' @examples
 #' crToSeurat(directory = "Files", min_cells = 3, min_features = 3)
@@ -11,24 +13,32 @@ crToSeurat <- function(directory,
                        min.cells = 3,
                        min.features = 200, 
                        sample.names = c("sample"), 
+                       gz = FALSE, 
                        merge = TRUE){ 
+  
+  components <- c("matrix.mtx", "features.tsv", "barcodes.tsv")
+  
+  if(isTRUE(gz)) {
+    components <- map_chr(components, ~str_c(.,".gz"))
+  } 
   
   path <- str_c(list.files(directory, full.names = TRUE), "filtered_gene_bc_matrices",
                 sep = "/")
   
-  matrix <- map(str_c(path,"matrix.mtx",sep = "/"), readMM)
-  features <- map(str_c(path,"features.tsv",sep = "/"), ~
+  matrix <- map(str_c(path,components[[1]],sep = "/"), readMM)
+  
+  features <- map(str_c(path,components[[2]],sep = "/"), ~
                     read_tsv(.x, col_names = FALSE, col_types = cols())$X2)
   
-  barcodes <- map(str_c(path,"barcodes.tsv",sep = "/"), ~
+  barcodes <- map(str_c(path,components[[3]],sep = "/"), ~
                     read_tsv(.x, col_names = FALSE, col_types = cols())$X1) 
   
-  matrix <- imap(matrix, ~ set_colnames(.x, barcodes[[.y]]) %>%
-                 set_rownames(.x, features[[.y]]) %>%
-                 CreateSeuratObject(.x, 
-                                    min.cells = min.cells,
-                                    project = sample.names[[.y]],
-                                    min.features = min.features))
+  matrix <- imap(matrix, ~ set_colnames(.x, barcodes[[.y]])) 
+  matrix <- imap(matrix, ~ set_rownames(.x, features[[.y]]))
+  matrix <- imap(matrix, ~ CreateSeuratObject(.x, 
+                                      min.cells = min.cells,
+                                      project = sample.names[[.y]],
+                                      min.features = min.features))
   
   if (length(matrix) == 1){ 
     matrix <- matrix[[1]]
